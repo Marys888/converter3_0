4bffc73fa07f152d1faf3db41b9b96e486a5b37a
Marys888 <severovamaha1999@gmail.com>
2025-04-12 09:32:20 +0200

    Add fahrenheit to celsius conversion

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,20 @@
 package app;
+import java.util.Scanner;
+
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Temperature measures converter App");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter temperature in Fahrenheit");
+        double temp = scanner.nextDouble();
+        System.out.println("Temperature in Celsius: " + " " + convertToCelsius(temp));
+        scanner.close();
     }
+    private static double convertToCelsius(double temp) {
+        return (temp - 32) * 5/9;
+
+    }
+
 }
